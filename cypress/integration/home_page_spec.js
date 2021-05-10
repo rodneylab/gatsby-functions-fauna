f4ccf450bc7b928b80f40f6ba9da4c66ab6d3762
Rodney Lab <codesign@rodneylab.com>
2021-05-10 10:37:14 +0100

    test(e2e): updated cypress tests

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,7 +3,7 @@ describe('/ accessibility checks', () => {
     cy.visit('/');
     cy.injectAxe();
     cy.log('Page header is rendered');
-    cy.findByRole('heading', { name: /Climate — Gatsby 3 Starter/i }).should('be.visible');
+    cy.findByRole('heading', { name: /Gatsby Functions Fauna Demo/i }).should('be.visible');
     cy.checkAccessibility();
   });
 });
