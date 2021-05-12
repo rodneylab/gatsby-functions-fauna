3fff316895b8f501ba70fd45b634a4e72b1ff50c
Rodney Lab <codesign@rodneylab.com>
2021-05-12 09:40:41 +0100

    fix: 💫 added back Akismet support after gatsby bug patch

--- src/api/submit-comment.js
+++ src/api/submit-comment.js
@@ -1,8 +1,10 @@
-// import { AkismetClient } from 'akismet-api';
+import { AkismetClient } from 'akismet-api';
 import axios from 'axios';
 import faunadb from 'faunadb';
 
-const createComment = async ({ name, parentCommentId, text, markedSpam, slug }) => {
+const createComment = async ({
+  name, parentCommentId, text, markedSpam, slug,
+}) => {
   const result = (async () => {
     try {
       const q = faunadb.query;
@@ -15,7 +17,7 @@ const createComment = async ({ name, parentCommentId, text, markedSpam, slug }) 
         q.Create(q.Collection(process.env.GATSBY_FAUNA_COLLECTION), {
           data: {
             date: new Date().toISOString(),
-            markedSpam: false,
+            markedSpam,
             name,
             parentCommentId,
             slug,
@@ -31,18 +33,21 @@ const createComment = async ({ name, parentCommentId, text, markedSpam, slug }) 
   return result;
 };
 
-// const spamCheck = async ({
-//   email, ip, name, text, userAgent,
-// }) => {
-//   const client = new AkismetClient({ key: AKISMET_API_KEY, blog: SITE_URL });
-//   return client.checkSpam({
-//     ip,
-//     useragent: userAgent,
-//     content: text,
-//     email,
-//     name,
-//   });
-// };
+const spamCheck = async ({
+  email, ip, name, text, userAgent,
+}) => {
+  const client = new AkismetClient({
+    key: process.env.AKISMET_API_KEY,
+    blog: process.env.SITE_URL,
+  });
+  return client.checkSpam({
+    user_ip: ip,
+    useragent: userAgent,
+    content: text,
+    email,
+    name,
+  });
+};
 
 const triggerRebuild = async () => {
   if (!process.env.GATSBY_CLOUD_SITE_ID) {
@@ -74,35 +79,43 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') {
     res.status(405).send('Method not allowed');
   } else {
-    const { email, name, parentCommentId, slug, text } = req.body;
+    const {
+      email, name, parentCommentId, slug, text,
+    } = req.body;
+    res.status(200).json(req.headers);
+    return;
     const ip = req.headers['client-ip'];
     const userAgent = req.headers['user-agent'];
     let markedSpam;
     let akismetError;
 
-    // try {
-    //   markedSpam = await spamCheck({
-    //     email, ip, text, userAgent,
-    //   });
-    // } catch (error) {
-    //   akismetError = error.message;
-    // }
+    try {
+      markedSpam = await spamCheck({
+        email,
+        ip,
+        text,
+        userAgent,
+      });
+    } catch (error) {
+      akismetError = error.message;
+    }
 
-    // if (akismetError) {
-    //   res.status(400).send(akismetError);
-    // } else {
-    const createCommentResult = await createComment({
-      name,
-      parentCommentId,
-      text,
-      markedSpam,
-      slug,
-    });
-    if (!createCommentResult.successful) {
-      res.status(400).send(createCommentResult.message);
+    if (akismetError) {
+      res.status(400).send(akismetError);
     } else {
-      await triggerRebuild();
-      res.status(200).send('All is well that ends well.');
+      const createCommentResult = await createComment({
+        name,
+        parentCommentId,
+        text,
+        markedSpam,
+        slug,
+      });
+      if (!createCommentResult.successful) {
+        res.status(400).send(createCommentResult.message);
+      } else {
+        await triggerRebuild();
+        res.status(200).send('All is well that ends well.');
+      }
     }
   }
 }
